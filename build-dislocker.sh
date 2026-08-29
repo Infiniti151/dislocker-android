dc13bde0dff875218740e64461d9d971f7d35a9e
Infiniti151 <43163551+Infiniti151@users.noreply.github.com>
2026-08-29 16:16:35 +0530

    docs(dislocker): add license file to package documentation

--- build-dislocker.sh
+++ build-dislocker.sh
@@ -207,9 +207,13 @@ rm -rf "$PKG_STAGE"
 mkdir -p \
     "$PKG_STAGE/$TERMUX_PREFIX/bin" \
     "$PKG_STAGE/$TERMUX_PREFIX/lib" \
+    "$PKG_STAGE/$TERMUX_PREFIX/share/doc/dislocker" \
     "$PKG_STAGE/DEBIAN" \
     "$APT_BINARY_DIR" \
     "$APT_POOL_DIR"
+
+# Copy the license into the package
+cp "$WORK_DIR/dislocker/LICENSE.txt" "$PKG_STAGE/$TERMUX_PREFIX/share/doc/dislocker/copyright"
 
 # ---------------------------------------------------------------------------
 # Copy binaries
